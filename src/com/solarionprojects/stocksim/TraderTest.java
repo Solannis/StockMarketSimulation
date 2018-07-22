a12013242c4fe60249196dd29a870220d289496f
mfreeman <mfreeman@localhost>
2018-07-22 00:16:29 -0700

    Added TraderTest JUnit class.

--- src/com/solarionprojects/stocksim/TraderTest.java
+++ src/com/solarionprojects/stocksim/TraderTest.java
@@ -16,6 +16,10 @@ import org.junit.jupiter.api.Test;
  *
  */
 class TraderTest {
+	
+	//=========================
+	// OBJECT GLOBAL CONSTANTS
+	//=========================
 
 	/**
 	 * @throws java.lang.Exception
@@ -45,9 +49,51 @@ class TraderTest {
 	void tearDown() throws Exception {
 	}
 
+	//-------------------
+	// TEST SET: traderID
+	//-------------------
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TestSetTraderID () {
+		//
+		// Tests to perform:
+		//	1 - Ensure parameter value is valid (positive, non-zero).
+		//	2 - Ensure traderID attribute can be set.
+		//	3 - Ensure traderID attribute, once set, cannot be reset.
+		//
+		
+		Trader t = new Trader();
+		
+		//
+		//	1 - Ensure parameter value is valid (positive, non-zero).
+		//
+		assertEquals(false, t.SetTraderID(SimConstants.INT_NEGATIVE));			// Parameter value cannot be negative
+		assertEquals(false, t.SetTraderID(SimConstants.INT_ZERO));				// Parameter value cannot be zero
+		//
+		//	2 - Ensure traderID attribute can be set.
+		//
+		assertEquals(true, t.SetTraderID(SimConstants.INT_POSITIVE));			// Should allow stockID to be set.
+		//
+		//	3 - Ensure traderID attribute, once set, cannot be reset.
+		//
+		assertEquals(false, t.SetTraderID(SimConstants.INT_POSITIVE));			// Should NOT allow stockID to be set.
 	}
+	
+	@Test
+	void TestGetTraderID () {
+		//
+		// Tests to perform:
+		//	1 - Ensure the correct traderID is returned.
+		//
+		
+		Trader t = new Trader();
+		t.SetTraderID(SimConstants.INT_POSITIVE);
+
+		//
+		//	1 - Ensure the correct symbol is returned.
+		//
+		assertEquals(SimConstants.INT_POSITIVE, t.GetTraderID());
+	}
+	
 
 }
